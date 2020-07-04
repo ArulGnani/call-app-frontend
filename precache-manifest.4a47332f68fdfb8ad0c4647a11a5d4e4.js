17b9cb63f7e8fa4a815800deee6aa14b0b15b363
ArulGnani <version47@gmail.com>
2020-07-04 21:03:38 +0530

    Updates

---
+++ precache-manifest.4a47332f68fdfb8ad0c4647a11a5d4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5f1a2686effa60beec3271ec134eca",
+    "url": "/call-app-frontend/index.html"
+  },
+  {
+    "revision": "46f2547797a2a53ccebd",
+    "url": "/call-app-frontend/static/css/main.d01ee0dd.chunk.css"
+  },
+  {
+    "revision": "492b1651bc358d9c5396",
+    "url": "/call-app-frontend/static/js/2.8efdcb3a.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/call-app-frontend/static/js/2.8efdcb3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f2547797a2a53ccebd",
+    "url": "/call-app-frontend/static/js/main.12465420.chunk.js"
+  },
+  {
+    "revision": "436acda616466d2f595a",
+    "url": "/call-app-frontend/static/js/runtime-main.3e916203.js"
+  }
+]);
